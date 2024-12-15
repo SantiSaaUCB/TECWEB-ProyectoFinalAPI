b3b66b5ce054d319ed3a8d2a940820cfe4c7e37e
SantiSaaUCB <santiago.saavedra@ucb.edu.bo>
2024-12-15 18:14:55 -0400

    Cambios

--- frontend/js/store.js
+++ frontend/js/store.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", async () => {
      await loadSongs();
- });
+});
+
 async function loadSongs() {
      try {
           const response = await fetch("https://tecweb-proyectofinalapi.onrender.com/api/songs");
           const songs = await response.json();
           const songsList = document.getElementById("songsList");
-          songsList.innerHTML = "";
+          songsList.innerHTML = ""; // Limpiar el contenido previo
+          
           songs.forEach((song) => {
                const songCard = document.createElement("div");
                songCard.classList.add("song-card");
                songCard.innerHTML = `
                     <div class="song-image-container">
-                         <img src="../css/img/disco.png" alt="Imagen de la canción" class="song-image">
+                         <img src="/img/disco.png" alt="Imagen de la canción" class="song-image">
                     </div>
                     <div>
                          <h3>${song.name}</h3>
